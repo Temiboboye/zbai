1213fe7080c6b0111059278c3279571bb819df79
temiboboye <temitayoboboye@gmail.com>
2026-03-20 05:19:29 +0000

    feat: add Google Analytics G-F2XQ4ZX0N4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,6 +105,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-F2XQ4ZX0N4" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-F2XQ4ZX0N4');`}
+        </Script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
         <script
           type="application/ld+json"
